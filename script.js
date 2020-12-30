3be50be529e22328c696485bb77ccc43e971b3ca
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-30 15:07:28 +0100

    return after crossing the border

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function snakeGoDown(){
         let element = document.getElementById("snakeTile"+i)
         let style = getComputedStyle(element)
         let numberCurrentTop = parseInt(style.top)
+        if(30+numberCurrentTop<300){
         document.getElementById("snakeTile"+i).style.top = (30+numberCurrentTop).toString()+"px"
+        }
+        else{document.getElementById("snakeTile"+i).style.top ="0px"}
     }
 }
 document.addEventListener("DOMContentLoaded", function(){
