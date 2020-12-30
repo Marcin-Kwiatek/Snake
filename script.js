09786ab4c3249d8d52a91fb7073577c4f991493a
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-30 14:59:05 +0100

    add two snake tiles moving down

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("js")
+function snakeGoDown(){
+    for(let i=1;i<=2;i++){
+        let element = document.getElementById("snakeTile"+i)
+        let style = getComputedStyle(element)
+        let numberCurrentTop = parseInt(style.top)
+        document.getElementById("snakeTile"+i).style.top = (30+numberCurrentTop).toString()+"px"
+    }
+}
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("now")
+    window.setInterval(snakeGoDown,1000)
+})
