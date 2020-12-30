b588d5c6e9f59e10f5ad79d6b4e58f114c841b9d
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-30 16:43:57 +0100

    find first tile

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function snakeGoDown(){
-    for(let i=1;i<=2;i++){
-        let element = document.getElementById("snakeTile"+i)
+function snakeGoDown() {
+    for (let i = 1; i <= 3; i++) {
+        let element = document.getElementById("snakeTile" + i)
         let style = getComputedStyle(element)
         let numberCurrentTop = parseInt(style.top)
-        if(30+numberCurrentTop<300){
-        document.getElementById("snakeTile"+i).style.top = (30+numberCurrentTop).toString()+"px"
+        if (30 + numberCurrentTop < 300) {
+            element.style.top = (30 + numberCurrentTop).toString() + "px"
         }
-        else{document.getElementById("snakeTile"+i).style.top ="0px"}
+        else { element.style.top = "0px" }
     }
 }
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("now")
-    window.setInterval(snakeGoDown,1000)
+
+function removeFirstTile(){
+    let resultNumberValues = []
+    let elements = document.getElementsByClassName("snakeTile")
+    for (i=0;i<elements.length;i++){
+    let valueSnakeTile = elements[i].id.replace('snakeTile','')
+    let numberValueSnakeTile = parseInt(valueSnakeTile)
+    resultNumberValues.push(numberValueSnakeTile)
+    }
+    let minValue = Math.min(...resultNumberValues)
+    
+}
+document.addEventListener("DOMContentLoaded", function () {
+    window.setInterval(snakeGoDown, 1000)
 })
